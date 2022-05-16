e18bd84e420fecca7ad326e718e0e928896bb8e0
Tasin Chowdhury <tasin.chowdhury1@gmail.com>
2022-05-16 14:14:43 -0400

    update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/Features",
+        features = "src/test/resources/Features/mortgagecalculator.feature",
         glue = {"step_definitions"},
         plugin = {"pretty", "summary", "me.jvt.cucumber.report.PrettyReports:target/cucumber-html-reports"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
